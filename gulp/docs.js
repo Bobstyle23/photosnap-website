ccdc7014b9647cf37d5af408e7e71df2a347aa2d
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2025-03-21 17:29:41 +0900

    Change back docs for gulp

--- gulp/docs.js
+++ gulp/docs.js
@@ -103,33 +103,31 @@ gulp.task("sass:docs", () => {
 // NOTE: copy images to docs
 
 gulp.task("images:docs", function () {
-  return (
-    gulp
-      .src(["./src/img/**/*", "!./src/img/svgicons/**/*"], { encoding: false })
-      .pipe(changed("./docs/img/"))
-      // .pipe(
-      //   imagemin([
-      //     imageminWebp({
-      //       quality: 85,
-      //     }),
-      //   ]),
-      // )
-      .pipe(extReplace(".webp"))
-      .pipe(gulp.dest("./docs/img/"))
-      .pipe(gulp.src("./src/img/**/*"))
-      .pipe(changed("./docs/img/"))
-      // .pipe(
-      //   imagemin(
-      //     [
-      //       imagemin.gifsicle({ interlaced: true }),
-      //       imagemin.mozjpeg({ quality: 85, progressive: true }),
-      //       imagemin.optipng({ optimizationLevel: 5 }),
-      //     ],
-      //     { verbose: true },
-      //   ),
-      // )
-      .pipe(gulp.dest("./docs/img/"))
-  );
+  return gulp
+    .src(["./src/img/**/*", "!./src/img/svgicons/**/*"], { encoding: false })
+    .pipe(changed("./docs/img/"))
+    .pipe(
+      imagemin([
+        imageminWebp({
+          quality: 85,
+        }),
+      ]),
+    )
+    .pipe(extReplace(".webp"))
+    .pipe(gulp.dest("./docs/img/"))
+    .pipe(gulp.src("./src/img/**/*"))
+    .pipe(changed("./docs/img/"))
+    .pipe(
+      imagemin(
+        [
+          imagemin.gifsicle({ interlaced: true }),
+          imagemin.mozjpeg({ quality: 85, progressive: true }),
+          imagemin.optipng({ optimizationLevel: 5 }),
+        ],
+        { verbose: true },
+      ),
+    )
+    .pipe(gulp.dest("./docs/img/"));
 });
 
 // NOTE: svg stack
